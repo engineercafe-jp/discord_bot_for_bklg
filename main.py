9a6d8554177be7e10bb7738d1c74c1ba684ea6ec
LAPTOP-O1I1SM4R\magrodooon <gpdj2001@gmail.com>
2024-06-21 14:54:14 +0900

    環境変数に関する記述の変更

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from keep_alive import keep_alive
 import smtplib
 from email.mime.text import MIMEText
 import ssl
+import os
 
 client = discord.Client(intents=discord.Intents.all())
 
 # webサーバー上の環境変数として設定したメアドを代入
-EMAIL_ADDRESS = MY_EMAIL_ADRESS
+EMAIL_ADDRESS = os.getenv("MY_EMAIL_ADRESS")
 
 @client.event
 async def on_ready():
